6cd6d88c3dc968e20e1698ea2a3aa58fcdcb34a5
Scott Walsh <scottkwalsh@gmail.com>
2012-02-12 12:41:13 +0000

    Add pdb. Some other changes

--- tag_views.py
+++ tag_views.py
@@ -8,6 +8,8 @@ def tag_detail(request, slug):
 	qs = TaggedItem.objects.get_by_model(Entry, tag)
 	return object_list(request, queryset=qs, extra_context={'tag':slug}, template_name='tags/detail.html')
 
+# This function appears not to be working correctly in pdb.. template not shown
+# Potential causes include; bad return format, bad template format, maybe more
 def all_tags(request):
 	qs = Tag.objects.all()
 	return object_list(request, queryset=qs, template_name='tags/list.html')
